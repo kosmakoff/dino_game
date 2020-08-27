da5f7f41dae34784602f3bb0f0f0e4f2922ce5eb
Oleg Kosmakov <kosmakoff@gmail.com>
2020-08-27 19:18:41 +0200

    Bouncy zero implemented, resolution fixed

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use ggez::event::{self};
 use ggez::{graphics, ContextBuilder, GameResult};
 
 fn main() -> GameResult {
-    let (ctx, event_loop) = &mut ContextBuilder::new("Dino Game", "Oleg Kosmakov").build()?;
+    let mut window_mode = ggez::conf::WindowMode::default();
+    window_mode.width = 640.0;
+    window_mode.height = 480.0;
 
-    graphics::set_drawable_size(ctx, 640.0, 480.0)?;
+    let (ctx, event_loop) = &mut ContextBuilder::new("Dino Game", "Oleg Kosmakov")
+        .window_mode(window_mode)
+        .build()?;
 
     let game = &mut Game::new(ctx)?;
     event::run(ctx, event_loop, game)
