a810206825f8f892a175f13f75f61c8ed82b3e96
Oleg Kosmakov <1533952+kosmakoff@users.noreply.github.com>
2020-08-27 13:02:40 +0200

    Redone with ggez, implemented image drawing and sound playing

--- src/game/sprites.rs
+++ src/game/sprites.rs
@@ -1,21 +1,21 @@
 use crate::game::digits::{load_digits, Digits};
-use image::imageops::FilterType;
-use image::{DynamicImage, GenericImageView};
+use ggez::error::GameResult;
+use ggez::Context;
 
 pub struct SpriteData {
     pub digits: Digits,
 }
 
 impl SpriteData {
-    pub fn new(scale: usize) -> Self {
+    pub fn new(ctx: &mut Context, _scale: usize) -> GameResult<Self> {
         let sprite_data = include_bytes!("../../assets/sprites.png");
 
         let sprite_image =
             image::load_from_memory_with_format(sprite_data, image::ImageFormat::Png)
                 .expect("Failed to load sprites");
 
-        SpriteData {
-            digits: load_digits(&sprite_image, [655, 2], [9, 11], 1),
-        }
+        Ok(SpriteData {
+            digits: load_digits(ctx, &sprite_image, [655, 2], [9, 11], 1)?,
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,14 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
-extern crate rodio;
-
 mod game;
 
-use crate::piston::AdvancedWindow;
-use crate::piston::PressEvent;
 use game::Game;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::*;
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use piston::window::WindowSettings;
-use rodio::Source;
-use std::io::BufReader;
-use std::io::Cursor;
-use std::io::Read;
+use ggez::event::{self};
+use ggez::{graphics, ContextBuilder, GameResult};
 
-fn main() {
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_2;
+fn main() -> GameResult {
+    let (ctx, event_loop) = &mut ContextBuilder::new("Dino Game", "Oleg Kosmakov").build()?;
 
-    // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("Dino Game", [640, 480])
-        .graphics_api(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+    graphics::set_drawable_size(ctx, 640.0, 480.0)?;
 
-    let mut app = Game::new(GlGraphics::new(opengl));
-
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(&args);
-        }
-
-        if let Some(args) = e.update_args() {
-            app.update(&args);
-            let dt = args.dt;
-            let fps = 1.0 / dt;
-            let fps_string = format!("Dino game. FPS: {:.1}", fps);
-            window.set_title(fps_string);
-        }
-
-        if let Some(Button::Keyboard(_)) = e.press_args() {
-            let score_reached_data = include_bytes!("../assets/sounds/score-reached.ogg");
-            let buf_reader = Cursor::new(score_reached_data.as_ref());
-            let source = rodio::Decoder::new(buf_reader).unwrap();
-            rodio::play_raw(&app.sound_device, source.convert_samples());
-        }
-    }
+    let game = &mut Game::new(ctx)?;
+    event::run(ctx, event_loop, game)
 }
