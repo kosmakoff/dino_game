32fede27171b7a539441145820b80718f6e4e314
Oleg Kosmakov <1533952+kosmakoff@users.noreply.github.com>
2020-08-28 11:22:02 +0200

    Dino bounces off edges of window

--- src/game/sprites.rs
+++ src/game/sprites.rs
@@ -1,9 +1,12 @@
 use crate::game::digits::{load_digits, Digits};
+use crate::game::helpers::get_image;
 use ggez::error::GameResult;
+use ggez::graphics::{FilterMode, Image};
 use ggez::Context;
 
 pub struct SpriteData {
     pub digits: Digits,
+    pub dino: Image,
 }
 
 impl SpriteData {
@@ -14,8 +17,12 @@ impl SpriteData {
             image::load_from_memory_with_format(sprite_data, image::ImageFormat::Png)
                 .expect("Failed to load sprites");
 
+        let mut dino_image = get_image(ctx, &sprite_image, [848, 2], [44, 47])?;
+        dino_image.set_filter(FilterMode::Nearest);
+
         Ok(SpriteData {
             digits: load_digits(ctx, &sprite_image, [655, 2], [9, 11], 1)?,
+            dino: dino_image,
         })
     }
 }
